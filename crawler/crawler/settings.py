84a552098b547792c803f3c5f9948f4c80979020
saikatsingha <saikatsingha@gmail.com>
2021-08-15 15:34:22 +0530

    DB connection

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -73,12 +73,36 @@ WSGI_APPLICATION = 'crawler.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crawler',
+        'USER': 'saikat1',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'myproject',
+#         'USER': 'myprojectuser',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 
 
 # Password validation
