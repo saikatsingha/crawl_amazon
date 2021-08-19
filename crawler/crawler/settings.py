f45079c3a6c468ede3c15b99964f1e7ce9552849
saikatsingha <saikatsingha@gmail.com>
2021-08-19 23:14:08 +0530

    Can not impliment template, need to start fresh

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'crawler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        #'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,10 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+STATICFILES_DIR = [BASE_DIR, 'crawler/static']
+
+STATIC_ROOT = [BASE_DIR, 'assets']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- crawler/crawler/urls.py
+++ crawler/crawler/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('mobile/', include('mobile.urls')),
     path('', include('mobile.urls')),
     path('admin/', admin.site.urls),
 ]
