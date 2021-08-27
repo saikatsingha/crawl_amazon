108a1cd51842b78ed17cb828a2bf327bdaece674
saikatsingha <saikatsingha@gmail.com>
2021-08-27 23:41:41 +0530

    Trying DB fetch in HTML

--- crawler/mobile/views.py
+++ crawler/mobile/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from data_crawl.models import Product_details, Product_type
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+
+    product = Product_details.objects.all()
+    # context = {
+    #     'product': product,
+    #     'company_name': company_name, 
+    #     'photo': photo, 
+    #     'description': description, 
+    #     'price': price,
+    #     'memory': memory, 
+    #     'display': display,
+    #     'camera': camera, 
+    #     'battery': battery, 
+    #     'processor': processor)  
+    # }
+    # Subject: Subjectsnames.objects.get(id=id)
+
+    # subjects_data: {
+    #     "subjects_names":Subject
+    # }
+    context = {
+        'product':product
+    }
+    print (product)
+    return render(request, 'index1.html', context)
 
